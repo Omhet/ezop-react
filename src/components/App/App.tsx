ebfb2d26c3289865039666bd5e7a5d5afabaec21
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-03-14 19:31:14 +0300

    InputBox component (draft)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import './style.scss';
 
 const App = () => {
-  return (
-    <div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
---
+++ src/components/InputBox/style.scss.d.ts
@@ -0,0 +1,16 @@
+declare namespace StyleScssModule {
+  export interface IStyleScss {
+    expandIcon: string;
+    header: string;
+    main: string;
+    name: string;
+    status: string;
+  }
+}
+
+declare const StyleScssModule: StyleScssModule.IStyleScss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: StyleScssModule.IStyleScss;
+};
+
+export = StyleScssModule;
