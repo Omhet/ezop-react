a239a4509b567edb7f80d46ec053fb6698891a47
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-03-16 11:36:54 +0300

    Add filler Logs containers

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,13 +3,17 @@ import QueryBox from '../../containers/QueryBox';
 import styles from './style.scss';
 import PageHeader from '../../containers/PageHeader';
 import OntologyBox from '../../containers/OntologyBox';
+import QueryLogs from '../../containers/QueryLogs';
+import OntologyLogs from '../../containers/OntologyLogs';
 
 const App = () => {
   return (
     <div className={styles.main}>
       <PageHeader />
       <OntologyBox />
+      <OntologyLogs />
       <QueryBox />
+      <QueryLogs />
     </div>
   );
 };
