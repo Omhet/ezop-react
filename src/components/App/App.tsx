17915d180ef8ef0d1118bfb040e3a0e4c49e8ef9
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-03-16 11:31:40 +0300

    Add filler OntologyBox container

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import QueryBox from '../../containers/QueryBox';
 import styles from './style.scss';
 import PageHeader from '../../containers/PageHeader';
+import OntologyBox from '../../containers/OntologyBox';
 
 const App = () => {
   return (
     <div className={styles.main}>
       <PageHeader />
+      <OntologyBox />
       <QueryBox />
     </div>
   );
---
+++ src/containers/OntologyBox.tsx
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import { RootState } from '../redux/rootReducer';
+import InputBox from '../components/InputBox/InputBox';
+import { Dispatch } from 'redux';
+import { ExecutionStatus } from '../types';
+
+const mapState = (state: RootState) => {
+  return {
+    name: 'Грамматика булевых выражений',
+    status: 'idle' as ExecutionStatus
+  };
+};
+
+const mapDispatch = (dispatch: Dispatch) => ({
+  onChange: (value: string) => {
+    console.log(value);
+  }
+});
+
+export default connect(mapState, mapDispatch)(InputBox);
