ec60720f74b2a90a4ed7379cb640a8f70594c728
Omhet <omhetehmo@yandex.ru>
2020-03-30 10:18:42 +0100

    Extend server data interface

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,20 @@
 export {};
 
+interface Ontology {
+  title: string;
+  author: string;
+  env: string;
+  text: string;
+  isDraft: boolean;
+}
+
 interface ServerData {
   curcnpt_id: string;
+  ontology: Ontology;
 }
 
 declare global {
   interface Window {
-    SERVER_DATA: ServerData;
+    serverData: ServerData;
   }
 }
